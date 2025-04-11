6f4f6b3cd5de1692f7689ddc543ddaad92df7f07
angie-kinya <angiekinya670@gmail.com>
2025-04-11 12:53:40 +0300

    Updated the excel exporting package.

--- taskpilot/app/Exports/TasksExport.php
+++ taskpilot/app/Exports/TasksExport.php
@@ -4,9 +4,12 @@ namespace App\Exports;
 
 use App\Models\Task;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\Exportable;
 
 class TasksExport implements FromCollection
 {
+    use Exportable;
+
     public function collection()
     {
         return Task::select('id', 'title', 'description', 'is_completed', 'created_at')->get();
